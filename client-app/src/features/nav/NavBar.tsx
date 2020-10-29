d4ddf6626bfeab263996c7f74253f7812d5fa6d9
Rajmund Polak <rajmund.polak@makolab.com>
2020-10-29 14:03:11 +0100

    Error check

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,5 +1,6 @@
 import { observer } from "mobx-react-lite";
 import React, { useContext } from "react";
+import { Link, NavLink } from "react-router-dom";
 import { Button, Container, Menu } from "semantic-ui-react";
 import ActivityStore from '../../stores/activityStore'
 
@@ -9,13 +10,17 @@ export const NavBar: React.FC = () => {
     <div>
       <Menu fixed="top" inverted>
         <Container>
-          <Menu.Item header>
+          <Menu.Item header as={NavLink} exact to='/'>
             <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}}/>
             Reactivities
           </Menu.Item>
-          <Menu.Item name='Activities' />
+          <Menu.Item name='Activities' as={NavLink} to='/activities' />
           <Menu.Item>
-              <Button onClick={activityStore.openCreateForm} positive content='Create Activity' />
+              <Button  
+              as={NavLink} to='/createActivity' 
+              positive 
+              content='Create Activity' 
+              />
           </Menu.Item>
         </Container>
       </Menu>
--- client-app/src/stores/activityStore.ts
+++ client-app/src/stores/activityStore.ts
@@ -15,7 +15,7 @@ configure({ enforceActions: "always" });
 export class ActivityStore {
   @observable activityRegistry = new Map();
   @observable activities: IActivity[] = [];
-  @observable selectedActivity: IActivity | undefined;
+  @observable activity: IActivity | undefined;
   @observable loadingInitial = false;
   @observable editMode = false;
   @observable submitting = false;
@@ -51,6 +51,32 @@ export class ActivityStore {
     }
   };
 
+  @action loadActivity = async (id: string) => {
+    let activity = this.getActivity(id);
+    if (activity) {
+      this.activity = activity;
+    }else{
+      
+      this.loadingInitial = true;
+      try {
+         activity = await agent.Activities.details(id);
+         runInAction(() => {
+           this.activity = activity;
+           this.loadingInitial = false;
+         })
+      } catch(error) {
+        runInAction(() => {
+          this.loadingInitial = false;
+        })
+        console.log(error);
+      }
+    }
+  };
+
+  getActivity = (id: string) => {
+    return this.activityRegistry.get(id);
+  };
+
   @action createActivity = async (activity: IActivity) => {
     this.submitting = true;
     try {
@@ -74,7 +100,7 @@ export class ActivityStore {
       await agent.Activities.update(activity);
       runInAction(() => {
         this.activityRegistry.set(activity.id, activity);
-        this.selectedActivity = activity;
+        this.activity = activity;
         this.editMode = false;
         this.submitting = false;
       });
@@ -110,16 +136,16 @@ export class ActivityStore {
 
   @action openCreateForm = () => {
     this.editMode = true;
-    this.selectedActivity = undefined;
+    this.activity = undefined;
   };
 
   @action openEditForm = (id: string) => {
-    this.selectedActivity = this.activityRegistry.get(id);
+    this.activity = this.activityRegistry.get(id);
     this.editMode = true;
   };
 
   @action cancelSelectedActivity = () => {
-    this.selectedActivity = undefined;
+    this.activity = undefined;
   };
 
   @action cancelFormOpen = () => {
@@ -127,7 +153,7 @@ export class ActivityStore {
   };
 
   @action selectActivity = (id: string) => {
-    this.selectedActivity = this.activityRegistry.get(id);
+    this.activity = this.activityRegistry.get(id);
     this.editMode = false;
   };
 }
